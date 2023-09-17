376d824f6be8d6a7a2a59593fa13c9e2a9ca6eab
Anna M <ankaanna@gmail.com>
2023-09-17 22:14:58 +0200

    rozwiazanie3.1.1.3 - poprawka stylu

--- src/main/java/pl/javastart/task/Furniture.java
+++ src/main/java/pl/javastart/task/Furniture.java
@@ -1,17 +1,21 @@
 package pl.javastart.task;
 
+import java.sql.SQLOutput;
+
 public class Furniture {
-        String type;
-        String colour;
-        double height;
-        public Furniture(String type, String colour, double height) {
-                this.type = type;
-                this.colour = colour;
-                this.height = height;
-        }
-        void showInfo () {
-            System.out.println("W pokoju znajduje się mebel: "
-                    + type + ", ma kolor: " + colour
-                    + " oraz wysokość: " + height);
-        }
+    String type;
+    String colour;
+    double height;
+
+    public Furniture(String type, String colour, double height) {
+        this.type = type;
+        this.colour = colour;
+        this.height = height;
+    }
+
+    void showInfo() {
+        System.out.println("W pokoju znajduje się mebel: "
+                 + type + ", ma kolor: " + colour
+                 + " oraz wysokość: " + height);
+    }
 }
